3333b0fa8e5265c57d792f090bcf598889805ccf
Mustafa <mchambqa@gmail.com>
2020-10-10 15:37:03 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,14 @@
-def countdown(num)
-    
-    while num > 0
-    puts "#{num} SECOND(S)!"
-    num -= 1
-    end
+
+def countdown(number)
+  
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  end
 
   return "HAPPY NEW YEAR!"
-  end
-  
-  def countdown_with_sleep(num)
-    sleep(3)
-  end
+end
+
+def countdown_with_sleep(number)
+  sleep(5)
+end
